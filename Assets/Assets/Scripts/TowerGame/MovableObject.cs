89b599ee1a4fbbb8631f14cfbb2d7b770047bcf6
Márcio Costa <115479794+MarcioCosta75@users.noreply.github.com>
2025-01-12 17:11:08 +0000

    First Person Controller Version Complete
    First Person Controller Version Complete

--- Assets/Assets/Scripts/TowerGame/MovableObject.cs
+++ Assets/Assets/Scripts/TowerGame/MovableObject.cs
@@ -3,20 +3,32 @@ using UnityEngine;
 public class MovableObject : MonoBehaviour
 {
     [SerializeField] private string correctTowerTag;
+    [SerializeField] private AudioClip correctSound;
+    [SerializeField] private AudioSource audioSource;
+
     private bool isPlaced = false;
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (isPlaced) return;
 
         if (other.CompareTag(correctTowerTag))
         {
+            PlaySound(correctSound);
+
             transform.position = other.transform.position + Vector3.up * other.bounds.size.y;
             transform.rotation = Quaternion.identity;
             isPlaced = true;
 
             ColorMatchGameManager.Instance.ObjectPlaced();
+        }
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        if (audioSource != null && clip != null)
+        {
+            audioSource.PlayOneShot(clip);
         }
     }
 }
